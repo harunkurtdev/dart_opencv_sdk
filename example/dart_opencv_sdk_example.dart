55b122e44b849aaf27aad428f7a216cfd4ad0939
harunlakodla <harunlakodla@gmail.com>
2023-06-18 22:06:32 +0300

    first ini

---
+++ example/dart_opencv_sdk_example.dart
@@ -0,0 +1,21 @@
+import 'package:dart_opencv_sdk/dart_opencv_sdk.dart';
+import 'package:image/image.dart' as img;
+
+void main() async {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+  
+  await (img.Command()
+        // Read a WebP image from a file.
+        ..decodeWebPFile('test.webp')
+        // Resize the image so its width is 120 and height maintains aspect
+        // ratio.
+        ..copyResize(width: 1200)
+        // ..grayscale()
+        ..drawCircle(
+            x: 20, y: 50, radius: 50, color: img.ColorFloat16.rgb(255, 0, 0))
+        // Save the image to a PNG file.
+        ..writeToFile('thumbnail_normalize.png'))
+      // Execute the image commands in an isolate thread
+      .executeThread();
+}
